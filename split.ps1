0bf6191e5cd00789228210d3b41dee35cb6cd28a
Patrick Kaak <pkaak@outlook.com>
2017-09-05 15:39:12 +0200

    Delete split.ps1

--- split.ps1
+++
@@ -1,5 +0,0 @@
-﻿# split string so import-module information is filtered out
-# this is done by filtering everything before <split> within the string
-
-$input = "string1<split>string2"
-$input -split "<split>" | select -Last 1
